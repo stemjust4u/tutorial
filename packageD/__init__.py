c3c47e043c4f8de7f5d859b19fe07ce1b19d0c86
stemjust4u <stemjust4u@gmail.com>
2021-03-15 20:15:59 -0600

    update

--- packageD/__init__.py
+++ packageD/__init__.py
@@ -1,4 +1,9 @@
+from .moduleD import *
+from .moduleD2 import *   
+from .moduleD3 import *
+
 __all__ = [
     'moduleD',
     'moduleD2',
+    'moduleD3',
 ]
--- top_scriptD.py
+++ top_scriptD.py
@@ -13,10 +13,12 @@ __all__ = [
 # So the functions can be called multiple ways
 
 # Full path
-from packageD import *
-print(moduleD.functionD(comment))
-print(moduleD2.functionD2(comment))
-print(moduleD3.functionD3(comment)) # will give error since moduleD3 not imported in init.py
+#from packageD import *
+import packageD
+print(packageD.functionD(comment))
+#print(moduleD.functionD(comment))
+#print(moduleD2.functionD2(comment))
+#print(moduleD3.functionD3(comment)) # will give error since moduleD3 not imported in init.py
 
 # Import a specific module as different name
 import packageD.moduleD3 as mm3
