5fe9cf7c66415a8314b45083be4cfc9d46b57d8e
stemjust4u <stemjust4u@gmail.com>
2021-03-17 08:56:55 -0600

    remove comment

--- packageB/__init__.py
+++ packageB/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py will be used when the package/folder is imported
 # the '.' dot indicates importing from current directory
-#from .moduleB import *  # import all functions/classes not starting with _ underscore
-#from .moduleB2 import *
+from .moduleB import *  # import all functions/classes not starting with _ underscore
+from .moduleB2 import *
 
 # Using * is convenient but not recommended. Both modules have a function called file_name that the user
 # would need to know before calling it in top_scriptB.py
