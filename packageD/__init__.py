cb6eca5820582d9ecc011c8fa2aaffa77834628c
stemjust4u <stemjust4u@gmail.com>
2021-05-03 16:09:37 -0600

    updated script D for all=[]

--- packageD/__init__.py
+++ packageD/__init__.py
@@ -1,12 +1,11 @@
-#from .moduleD import *
-#from .moduleD2 import *   
-#from .moduleD3 import *
+from .moduleD import *
+from .moduleD2 import *   
+from .moduleD3 import *
 #import packageD.moduleD
 #import packageD.moduleD2
 #import packageD.moduleD3
 
 __all__ = [
     'moduleD',
-    'moduleD2',
-    'moduleD3'
+    'functionD'
 ]
--- top_scriptD.py
+++ top_scriptD.py
@@ -6,31 +6,15 @@ comment = "executing "
 '''
 __all__ = [
     'moduleD',
-    'moduleD2',
+    'functionD'
 ]
 '''
-
-# So the functions can be called multiple ways
-
-# Full path
-#from packageD import *
-#import packageD
-#print(packageD.moduleD.functionD(comment))
-#print(moduleD.functionD(comment))
-#print(moduleD2.functionD2(comment))
-#print(moduleD3.functionD3(comment))
-
-#print(moduleD2.functionD2(comment))
-#print(moduleD3.functionD3(comment)) # will give error since moduleD3 not imported in init.py
-
-# Import a specific module as different name
-import packageD.moduleD3 as moduleD3
-print(moduleD3.functionD3(comment))
+#Both moduleD and functionD are made visible
 
 from packageD import *
 print(moduleD.functionD(comment))
-print(moduleD2.functionD2(comment))
-print(moduleD3.functionD3(comment))
+#print(moduleD2.functionD2(comment))
+#print(moduleD3.functionD3(comment))
 
 from packageD import moduleD
-print(moduleD.functionD(comment))
+print(functionD(comment))
